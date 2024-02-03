f5c01b5e31f8c815076bffb906317e85ec2a7727
raynerng <rayner_ng1997@live.com>
2024-02-04 00:58:17 +0800

    added timezone functionality with labels

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,23 @@
-import logo from "/logo.png";
+import time from "/time.jpg";
 import "./App.css";
+import Clock from "./Clock.jsx";
 
 function App() {
   return (
     <>
       <div>
-        <img src={logo} className="logo" alt="Rocket logo" />
+        <img src={time} className="logo" alt="time logo" />
+        <p>This image is generated using Bard.</p>
       </div>
       <h1>World Clock</h1>
       <div className="card">
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
+        <Clock timeZone="US/Pacific" />
+        <Clock timeZone="Asia/Singapore" />
+        <Clock timeZone="Japan" />
+        <p>{"Hello world"}</p>
       </div>
     </>
   );
---
+++ src/Clock.jsx
@@ -0,0 +1,16 @@
+import { useEffect, useState } from "react";
+
+export default function Clock({ timeZone }) {
+  const [date, setDate] = useState(new Date());
+
+  useEffect(() => {
+    setTimeout(() => setDate(new Date()), 1000);
+  }, [date]);
+
+  // return <p>{date.toLocaleString("en-GB", { timeZone: timeZone })}</p>;
+  return (
+    <p>{`${date.toLocaleString("en-GB", {
+      timeZone: timeZone,
+    })} (Time in ${timeZone})`}</p>
+  );
+}
