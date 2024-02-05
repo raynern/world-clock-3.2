a22dd7eeef09fdb498cda01944998606dffa1336
raynerng <rayner_ng1997@live.com>
2024-02-05 21:11:17 +0800

    fixed base url

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "world-clock-3.2",
+  base: "/world-clock-3.2/",
 });
